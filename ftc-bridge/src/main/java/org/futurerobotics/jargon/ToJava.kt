30a868ab5a80473c48ed15b619ebd001c95b5186
Benjamin Ye <BobBenBan@users.noreply.github.com>
2019-12-06 16:14:54 -0800

    Fix a few things

--- ftc-bridge/src/main/java/org/futurerobotics/jargon/ToJava.kt
+++
@@ -1,8 +0,0 @@
-package org.futurerobotics.jargon
-
-/**
- * returns `this` as a java.lang.Object.
- */
-@Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-inline val Any.javaObj: Object
-    get() = this as Object
